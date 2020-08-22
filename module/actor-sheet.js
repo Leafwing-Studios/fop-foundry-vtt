7e7872025eaa3099796608f803a4768822a55365
Alice Cecile <alice.i.cecile@gmail.com>
2020-08-22 00:23:21 -0400

    Linting

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -3,16 +3,21 @@
  * @extends {ActorSheet}
  */
 export class SimpleActorSheet extends ActorSheet {
-
   /** @override */
-	static get defaultOptions() {
-	  return mergeObject(super.defaultOptions, {
-  	  classes: ["worldbuilding", "sheet", "actor"],
-  	  template: "systems/worldbuilding/templates/actor-sheet.html",
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["worldbuilding", "sheet", "actor"],
+      template: "systems/worldbuilding/templates/actor-sheet.html",
       width: 600,
       height: 600,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
-      dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "description",
+        },
+      ],
+      dragDrop: [{ dragSelector: ".item-list .item", dropSelector: null }],
     });
   }
 
@@ -22,7 +27,7 @@ export class SimpleActorSheet extends ActorSheet {
   getData() {
     const data = super.getData();
     data.dtypes = ["String", "Number", "Boolean"];
-    for ( let attr of Object.values(data.data.attributes) ) {
+    for (let attr of Object.values(data.data.attributes)) {
       attr.isCheckbox = attr.dtype === "Boolean";
     }
     return data;
@@ -31,34 +36,40 @@ export class SimpleActorSheet extends ActorSheet {
   /* -------------------------------------------- */
 
   /** @override */
-	activateListeners(html) {
+  activateListeners(html) {
     super.activateListeners(html);
 
     // Everything below here is only needed if the sheet is editable
     if (!this.options.editable) return;
 
     // Update Inventory Item
-    html.find('.item-edit').click(ev => {
+    html.find(".item-edit").click((ev) => {
       const li = $(ev.currentTarget).parents(".item");
       const item = this.actor.getOwnedItem(li.data("itemId"));
       item.sheet.render(true);
     });
 
     // Delete Inventory Item
-    html.find('.item-delete').click(ev => {
+    html.find(".item-delete").click((ev) => {
       const li = $(ev.currentTarget).parents(".item");
       this.actor.deleteOwnedItem(li.data("itemId"));
       li.slideUp(200, () => this.render(false));
     });
 
     // Add or Remove Attribute
-    html.find(".attributes").on("click", ".attribute-control", this._onClickAttributeControl.bind(this));
+    html
+      .find(".attributes")
+      .on(
+        "click",
+        ".attribute-control",
+        this._onClickAttributeControl.bind(this)
+      );
   }
 
   /* -------------------------------------------- */
 
   /** @override */
-  setPosition(options={}) {
+  setPosition(options = {}) {
     const position = super.setPosition(options);
     const sheetBody = this.element.find(".sheet-body");
     const bodyHeight = position.height - 192;
@@ -81,7 +92,7 @@ export class SimpleActorSheet extends ActorSheet {
     const form = this.form;
 
     // Add new attribute
-    if ( action === "create" ) {
+    if (action === "create") {
       const nk = Object.keys(attrs).length + 1;
       let newKey = document.createElement("div");
       newKey.innerHTML = `<input type="text" name="data.attributes.attr${nk}.key" value="attr${nk}"/>`;
@@ -91,7 +102,7 @@ export class SimpleActorSheet extends ActorSheet {
     }
 
     // Remove existing attribute
-    else if ( action === "delete" ) {
+    else if (action === "delete") {
       const li = a.closest(".attribute");
       li.parentElement.removeChild(li);
       await this._onSubmit(event);
@@ -102,28 +113,35 @@ export class SimpleActorSheet extends ActorSheet {
 
   /** @override */
   _updateObject(event, formData) {
-
     // Handle the free-form attributes list
     const formAttrs = expandObject(formData).data.attributes || {};
     const attributes = Object.values(formAttrs).reduce((obj, v) => {
       let k = v["key"].trim();
-      if ( /[\s\.]/.test(k) )  return ui.notifications.error("Attribute keys may not contain spaces or periods");
+      if (/[\s\.]/.test(k))
+        return ui.notifications.error(
+          "Attribute keys may not contain spaces or periods"
+        );
       delete v["key"];
       obj[k] = v;
       return obj;
     }, {});
-    
+
     // Remove attributes which are no longer used
-    for ( let k of Object.keys(this.object.data.data.attributes) ) {
-      if ( !attributes.hasOwnProperty(k) ) attributes[`-=${k}`] = null;
+    for (let k of Object.keys(this.object.data.data.attributes)) {
+      if (!attributes.hasOwnProperty(k)) attributes[`-=${k}`] = null;
     }
 
     // Re-combine formData
-    formData = Object.entries(formData).filter(e => !e[0].startsWith("data.attributes")).reduce((obj, e) => {
-      obj[e[0]] = e[1];
-      return obj;
-    }, {_id: this.object._id, "data.attributes": attributes});
-    
+    formData = Object.entries(formData)
+      .filter((e) => !e[0].startsWith("data.attributes"))
+      .reduce(
+        (obj, e) => {
+          obj[e[0]] = e[1];
+          return obj;
+        },
+        { _id: this.object._id, "data.attributes": attributes }
+      );
+
     // Update the Actor
     return this.object.update(formData);
   }
--- module/item-sheet.js
+++ module/item-sheet.js
@@ -3,16 +3,21 @@
  * @extends {ItemSheet}
  */
 export class SimpleItemSheet extends ItemSheet {
-
   /** @override */
-	static get defaultOptions() {
-	  return mergeObject(super.defaultOptions, {
-			classes: ["worldbuilding", "sheet", "item"],
-			template: "systems/worldbuilding/templates/item-sheet.html",
-			width: 520,
-			height: 480,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}]
-		});
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["worldbuilding", "sheet", "item"],
+      template: "systems/worldbuilding/templates/item-sheet.html",
+      width: 520,
+      height: 480,
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "description",
+        },
+      ],
+    });
   }
 
   /* -------------------------------------------- */
@@ -21,7 +26,7 @@ export class SimpleItemSheet extends ItemSheet {
   getData() {
     const data = super.getData();
     data.dtypes = ["String", "Number", "Boolean"];
-    for ( let attr of Object.values(data.data.attributes) ) {
+    for (let attr of Object.values(data.data.attributes)) {
       attr.isCheckbox = attr.dtype === "Boolean";
     }
     return data;
@@ -30,7 +35,7 @@ export class SimpleItemSheet extends ItemSheet {
   /* -------------------------------------------- */
 
   /** @override */
-  setPosition(options={}) {
+  setPosition(options = {}) {
     const position = super.setPosition(options);
     const sheetBody = this.element.find(".sheet-body");
     const bodyHeight = position.height - 192;
@@ -41,14 +46,20 @@ export class SimpleItemSheet extends ItemSheet {
   /* -------------------------------------------- */
 
   /** @override */
-	activateListeners(html) {
+  activateListeners(html) {
     super.activateListeners(html);
 
     // Everything below here is only needed if the sheet is editable
     if (!this.options.editable) return;
 
     // Add or Remove Attribute
-    html.find(".attributes").on("click", ".attribute-control", this._onClickAttributeControl.bind(this));
+    html
+      .find(".attributes")
+      .on(
+        "click",
+        ".attribute-control",
+        this._onClickAttributeControl.bind(this)
+      );
   }
 
   /* -------------------------------------------- */
@@ -66,7 +77,7 @@ export class SimpleItemSheet extends ItemSheet {
     const form = this.form;
 
     // Add new attribute
-    if ( action === "create" ) {
+    if (action === "create") {
       const nk = Object.keys(attrs).length + 1;
       let newKey = document.createElement("div");
       newKey.innerHTML = `<input type="text" name="data.attributes.attr${nk}.key" value="attr${nk}"/>`;
@@ -76,7 +87,7 @@ export class SimpleItemSheet extends ItemSheet {
     }
 
     // Remove existing attribute
-    else if ( action === "delete" ) {
+    else if (action === "delete") {
       const li = a.closest(".attribute");
       li.parentElement.removeChild(li);
       await this._onSubmit(event);
@@ -87,27 +98,34 @@ export class SimpleItemSheet extends ItemSheet {
 
   /** @override */
   _updateObject(event, formData) {
-
     // Handle the free-form attributes list
     const formAttrs = expandObject(formData).data.attributes || {};
     const attributes = Object.values(formAttrs).reduce((obj, v) => {
       let k = v["key"].trim();
-      if ( /[\s\.]/.test(k) )  return ui.notifications.error("Attribute keys may not contain spaces or periods");
+      if (/[\s\.]/.test(k))
+        return ui.notifications.error(
+          "Attribute keys may not contain spaces or periods"
+        );
       delete v["key"];
       obj[k] = v;
       return obj;
     }, {});
-    
+
     // Remove attributes which are no longer used
-    for ( let k of Object.keys(this.object.data.data.attributes) ) {
-      if ( !attributes.hasOwnProperty(k) ) attributes[`-=${k}`] = null;
+    for (let k of Object.keys(this.object.data.data.attributes)) {
+      if (!attributes.hasOwnProperty(k)) attributes[`-=${k}`] = null;
     }
 
     // Re-combine formData
-    formData = Object.entries(formData).filter(e => !e[0].startsWith("data.attributes")).reduce((obj, e) => {
-      obj[e[0]] = e[1];
-      return obj;
-    }, {_id: this.object._id, "data.attributes": attributes});
+    formData = Object.entries(formData)
+      .filter((e) => !e[0].startsWith("data.attributes"))
+      .reduce(
+        (obj, e) => {
+          obj[e[0]] = e[1];
+          return obj;
+        },
+        { _id: this.object._id, "data.attributes": attributes }
+      );
 
     // Update the Item
     return this.object.update(formData);
